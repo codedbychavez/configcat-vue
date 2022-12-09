a4ce640a6dcfb3514901038c12fb3af65c5d5de1
Chavez Harris <codedbychavez@gmail>
2022-12-08 22:11:02 -0300

    Added props validation

--- src/components/FeatureWrapper.vue
+++ src/components/FeatureWrapper.vue
@@ -1,3 +1,15 @@
 <template>
   <!-- Added feature wrapper -->
 </template>
+
+<script>
+export default {
+  props: {
+    featureKey: {
+      type: String,
+      required: true,
+    }
+  }
+}
+
+</script>
