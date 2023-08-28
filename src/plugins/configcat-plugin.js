4259fdf6d1c350d6946a2efa05b65e5ac2cf2699
Adam Simon <adamosimoni@gmail.com>
2023-08-28 18:17:32 +0200

    Fix ConfigCat client initialization & cleanup

--- src/plugins/configcat-plugin.js
+++ src/plugins/configcat-plugin.js
@@ -3,31 +3,31 @@ import * as configcat from 'configcat-js-ssr';
 export default {
   install: (app, options) => {
 
-     // Auto poll is default
-     let configCatClient = configcat.getClient(
-      options.SDKKey,
-      configcat.PollingMode.AutoPoll, 
-      options.clientOptions
-    )
-
-    if (options.pollingMode == 'manual') {
+    let pollingMode =
       // https://configcat.com/docs/sdk-reference/js/#manual-polling
-      configCatClient = configcat.getClient(
-        options.SDKKey,
-        configcat.PollingMode.ManualPoll, 
-        options.clientOptions
-      );
-    }
-
-    if (options.pollingMode == 'lazy') {
+      options.pollingMode === 'manual' ? configcat.PollingMode.ManualPoll :
       // https://configcat.com/docs/sdk-reference/js/#lazy-loading
-      configCatClient = configcat.getClient(
-        options.SDKKey,
-        configcat.PollingMode.LazyLoad, 
-        options.clientOptions
-      );
-    }
+      options.pollingMode === 'lazy' ? configcat.PollingMode.LazyLoad :
+      // Auto poll is default
+      configcat.PollingMode.AutoPoll;
+
+    let configCatClient = configcat.getClient(
+      options.SDKKey,
+      pollingMode, 
+      options.clientOptions
+    );
 
     app.config.globalProperties.configCatClient = configCatClient;
+
+    // The acquired `configCatClient` object should be active as long as the Vue app is alive (but no longer than that).
+    // However, Vue doesn't expose an API currently which would allow us to hook into the lifecycle of the app component.
+    // The recommended workaround is to wrap its `unmount` method
+    // (see https://github.com/vuejs/core/issues/4516#issuecomment-913231053).
+    const originalAppUnmount = app.unmount;
+    app.unmount = function() {
+      originalAppUnmount.apply(this, arguments);
+      configCatClient.dispose();
+    }
+
   },
 };
