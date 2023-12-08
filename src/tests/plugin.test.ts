a4a8b731f926bc082e8131052ef8e7c2ea97b0b4
Chavez Harris <codedbychavez@gmail.com>
2023-12-08 18:43:30 -0300

    Added plugin test cases

---
+++ src/tests/plugin.test.ts
@@ -0,0 +1,73 @@
+import { createApp, h } from "vue";
+import { test, expect } from "vitest";
+
+import ConfigCatPlugin from "../plugins/ConfigCatPlugin";
+import { PollingMode, createConsoleLogger, LogLevel } from "../index";
+
+test("installs without errors", () => {
+  const app = createApp({
+    render: () => h("div"),
+  });
+
+  expect(() => {
+    app.use(ConfigCatPlugin, {
+      sdkKey: "YOUR-CONFIGCAT-SDK-KEY",
+    });
+  }).not.toThrow();
+});
+
+test("throws error when sdkKey is not provided", () => {
+  const app = createApp({
+    render: () => h("div"),
+  });
+
+  expect(() => {
+    app.use(ConfigCatPlugin, {});
+  }).toThrow();
+});
+
+test("the plugin installs when a polling mode is specified", () => {
+  const app = createApp({
+    render: () => h("div"),
+  });
+
+  expect(() => {
+    app.use(ConfigCatPlugin, {
+      sdkKey: "YOUR-CONFIGCAT-SDK-KEY",
+      pollingMode: PollingMode.AutoPoll,
+    });
+  }).not.toThrow();
+});
+
+test("the plugin installs when a logger is specified", () => {
+  const app = createApp({
+    render: () => h("div"),
+  });
+
+  expect(() => {
+    app.use(ConfigCatPlugin, {
+      sdkKey: "YOUR-CONFIGCAT-SDK-KEY",
+      pollingMode: PollingMode.AutoPoll,
+      clientOptions: {
+        logger: createConsoleLogger(LogLevel.Info),
+      },
+    });
+  }).not.toThrow();
+});
+
+test("the plugin installs when a pollingIntervalSeconds is specified", () => {
+  const app = createApp({
+    render: () => h("div"),
+  });
+
+  expect(() => {
+    app.use(ConfigCatPlugin, {
+      sdkKey: "YOUR-CONFIGCAT-SDK-KEY",
+      pollingMode: PollingMode.AutoPoll,
+      clientOptions: {
+        logger: createConsoleLogger(LogLevel.Info),
+        pollIntervalSeconds: 5,
+      },
+    });
+  }).not.toThrow();
+});
