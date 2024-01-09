f3a6505950756862d2e32ad2b13e25e7bc5ef61f
Chavez Harris <codedbychavez@gmail.com>
2024-01-09 16:22:33 -0300

    dist update

--- dist/plugins/LocalStorageCache.d.ts
+++ dist/plugins/LocalStorageCache.d.ts
@@ -1,5 +1,11 @@
-import type { IConfigCatCache } from "configcat-common";
+import type { IConfigCatCache, IConfigCatKernel } from "configcat-common";
 export declare class LocalStorageCache implements IConfigCatCache {
+    private readonly storage;
+    static setup(kernel: IConfigCatKernel, localStorageGetter?: () => Storage | null): IConfigCatKernel;
+    constructor(storage: Storage);
     set(key: string, value: string): void;
     get(key: string): string | undefined;
 }
+export declare function getLocalStorage(): Storage | null;
+export declare function toUtf8Base64(str: string): string;
+export declare function fromUtf8Base64(str: string): string;
