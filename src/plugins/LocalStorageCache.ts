09d6699b73161cbcc4a62e44ab2882e2c1c8b1ad
Adam Simon <adamosimoni@gmail.com>
2024-01-09 15:55:53 +0100

    Fix bug when cache payload contains characters outside the ASCII range

--- src/plugins/LocalStorageCache.ts
+++ src/plugins/LocalStorageCache.ts
@@ -3,7 +3,7 @@ import type { IConfigCatCache } from "configcat-common";
 export class LocalStorageCache implements IConfigCatCache {
   set(key: string, value: string): void {
     try {
-      localStorage.setItem(key, btoa(value));
+      localStorage.setItem(key, toUtf8Base64(value));
     } catch (ex) {
       // local storage is unavailable
     }
@@ -13,11 +13,23 @@ export class LocalStorageCache implements IConfigCatCache {
     try {
       const configString = localStorage.getItem(key);
       if (configString) {
-        return atob(configString);
+        return fromUtf8Base64(configString);
       }
     } catch (ex) {
-      // local storage is unavailable or invalid cache value in local storage
+      // local storage is unavailable or invalid cache value in localstorage
     }
     return void 0;
   }
+}
+
+export function toUtf8Base64(str: string): string {
+  str = encodeURIComponent(str);
+  str = str.replace(/%([0-9A-F]{2})/g, (_, p1) => String.fromCharCode(parseInt(p1, 16)));
+  return btoa(str);
+}
+
+export function fromUtf8Base64(str: string): string {
+  str = atob(str);
+  str = str.replace(/[%\x80-\xFF]/g, m => "%" + m.charCodeAt(0).toString(16));
+  return decodeURIComponent(str);
 }
