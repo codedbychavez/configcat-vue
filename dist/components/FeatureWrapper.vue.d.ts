4fd521813b553b85717f75a5af67c526d75c0740
Chavez Harris <codedbychavez@gmail.com>
2025-04-01 14:05:44 -0400

    Updates

--- dist/components/FeatureWrapper.vue.d.ts
+++ dist/components/FeatureWrapper.vue.d.ts
@@ -1,23 +1,26 @@
 import { User } from '@configcat/sdk';
-declare function __VLS_template(): {
-    default?(_: {}): any;
-    else?(_: {}): any;
-    loading?(_: {}): any;
+type __VLS_Props = {
+    featureKey: string;
+    userObject?: User;
 };
-declare const __VLS_component: import('vue').DefineComponent<{
-    featureKey: string;
-    userObject?: User;
-}, {}, {}, {}, {}, import('vue').ComponentOptionsMixin, import('vue').ComponentOptionsMixin, {
+declare function __VLS_template(): {
+    attrs: Partial<{}>;
+    slots: {
+        default?(_: {}): any;
+        else?(_: {}): any;
+        loading?(_: {}): any;
+    };
+    refs: {};
+    rootEl: any;
+};
+type __VLS_TemplateResult = ReturnType<typeof __VLS_template>;
+declare const __VLS_component: import('vue').DefineComponent<__VLS_Props, {}, {}, {}, {}, import('vue').ComponentOptionsMixin, import('vue').ComponentOptionsMixin, {} & {
     flagValueChanged: (newValue: boolean) => any;
-}, string, import('vue').PublicProps, Readonly<{
-    featureKey: string;
-    userObject?: User;
-}> & Readonly<{
+}, string, import('vue').PublicProps, Readonly<__VLS_Props> & Readonly<{
     onFlagValueChanged?: ((newValue: boolean) => any) | undefined;
 }>, {}, {}, {}, {}, string, import('vue').ComponentProvideOptions, false, {}, any>;
-declare const _default: __VLS_WithTemplateSlots<typeof __VLS_component, ReturnType<typeof __VLS_template>>;
+declare const _default: __VLS_WithTemplateSlots<typeof __VLS_component, __VLS_TemplateResult["slots"]>;
 export default _default;
-
 type __VLS_WithTemplateSlots<T, S> = T & {
     new (): {
         $slots: S;
